f4a239566db4d025a64e68460aac87c5adb23bef
haihuanchen <haihuan.chen@hotmail.com>
2020-04-02 10:28:02 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //get the form on html
+  //add event listener submit 
+  //prevent default
+  //grab whatever user submits
+  //implement into DOM
+  const newTaskForm = document.querySelector('#create-task-form')
+  const newTaskDesc = document.querySelector('#new-task-description')
+  const list = document.querySelector('#tasks')
+
+  newTaskForm.addEventListener('submit', createNewTask)
+
+  function createNewTask(event){
+    event.preventDefault();
+    const li = document.createElement('li')
+    li.innerText = newTaskDesc.value
+    const button = document.createElement('button')
+    button.innerText = 'x'
+    list.appendChild(li)
+    li.append(button)
+    event.target.reset();
+    
+    button.addEventListener('click',function(event){
+      // button.parentNode.remove()
+     event.target.parentNode.remove()
+
+    })
+  }
+
 });
